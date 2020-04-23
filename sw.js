a9336f8660a1ce55f37fa413fcc66ec528660af4
winwangqi <winwangqi@users.noreply.github.com>
2020-04-23 16:28:12 +0000

    Automated deployment: Thu Apr 23 16:28:11 UTC 2020 winwangqi/notebook_website@613a61d16f45c73b817da8119d0ade1d7c86b3ad

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ccabca9f1b7af5e45d70.js"
+    "url": "webpack-runtime-01e11782c965f3273dc1.js"
   },
   {
     "url": "styles.c3bfc5f693e517be8197.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "52cb4eb439531c5af4a2fc6191b26d8c"
+    "revision": "0c1b68d50560befcf2d11bfe5e78fa5f"
   },
   {
     "url": "manifest.webmanifest",
